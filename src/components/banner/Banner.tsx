0bbd6b68f512b3df268c13e0a63f0971a11af6b3
Sebas Cuervo <sebas.cuervo@experimentality.co>
2021-03-08 10:54:39 -0500

    add banner text

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,5 +1,5 @@
 import "./Banner.scss";
-import React from "react";
+import React, { Fragment } from "react";
 import Slider from "react-slick";
 
 
@@ -25,7 +25,7 @@ export default function Banner() {
     };
 
     const banners = [
-      {image:'https://images.unsplash.com/photo-1615081423479-1e819ec2853e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80' , text: 'numero 1'},
+      {image:'https://images.unsplash.com/photo-1615081423479-1e819ec2853e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80' , text: '"Una mujer debe ser dos cosas: quien ella quiera y lo que ella quiera. Y ustedes eligieron ser unas tesas"'},
       {image:'https://images.unsplash.com/photo-1611095567319-2f4c389168a9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80' , text: 'numero 1'},
       {image:'https://images.unsplash.com/photo-1615081423479-1e819ec2853e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80' , text: 'numero 1'},
     ]
@@ -34,10 +34,14 @@ export default function Banner() {
     <div className="banner-container">
       <Slider {...settings}>
           {banners && banners.map((banner, index) => (
-            <div  className="banner-container__slide">
-              <img src={banner.image} alt=""/>
-              <h2 >{banner.text}</h2>
-            </div>
+           <Fragment>
+             
+              <div  className="banner-container__slide">
+                <img src={banner.image} alt=""/>
+                <h2>{banner.text}</h2>
+              </div>
+              
+           </Fragment>
           ))}
         </Slider>
     </div>
